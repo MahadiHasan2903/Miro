5223166551a29a43ffd13b155ae731a39f18bd34
MD. Mahadi Hasan <12mahadihasan@gmail.com>
2024-03-02 19:06:35 +0600

    completed Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,90 @@
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaYoutubeSquare } from "react-icons/fa";
+import newsletter from "../assets/images/newslettter-bg.png";
+
 const Footer = () => {
   return (
-    <div className="flex justify-between w-full mx-5 mt-12 text-white bg-primary">
-      <div className="flex flex-col ">
-        <div className="flex items-start justify-between">
-          <div>1</div>
-          <div>2</div>
+    <div className="mt-[100px] bg-primary">
+      <div className="container flex justify-between w-full p-12 mt-5 text-white">
+        <div className="flex flex-col w-1/2 mt-12">
+          <div className="flex items-start justify-between gap-x-5 ">
+            <div>
+              <p className=" text-[23px] font-extrabold">
+                Scan. Detect. Remove.
+              </p>
+              <div className="flex items-center my-8 gap-x-8">
+                <FaTwitterSquare size={30} className="cursor-pointer" />
+                <FaSquareFacebook size={30} className="cursor-pointer" />
+                <FaYoutubeSquare size={30} className="cursor-pointer" />
+              </div>
+              <div className="flex items-center justify-between my-8">
+                <p className="text-[11px] opacity-60 underline cursor-pointer">
+                  Privacy Policy
+                </p>
+                <p className="text-[11px] opacity-60 underline cursor-pointer">
+                  Terms of Services
+                </p>
+              </div>
+              <p className="text-[11px] opacity-60 my-8">
+                Copyright © 2024 Certo Software Limited | Registered in <br />
+                England & Wales No. 10072356
+              </p>
+
+              <p className="text-[11px] opacity-60 my-5">
+                Designed & developed by Md. Mahadi Hasan
+              </p>
+            </div>
+            <div>
+              <p className="text-[35px] font-extrabold mb-8">Miro.</p>
+              <hr className="w-[150px] opacity-60" />
+
+              <div className="text-tertiary text-[19px] font-extrabold my-6 ">
+                <p className="mt-3">- iPhone</p>
+                <p className="mt-3">- Android</p>
+                <p className="mt-3">- Help</p>
+                <p className="mt-3">- About</p>
+                <p className="mt-3">- Insights</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-10 mb-5 mr-10">
+            <p className="text-[12px]   opacity-60">
+              Apple, the Apple logo, and iPhone are trademarks of Apple Inc.,
+              registered in the U.S. and other countries. App Store is a service
+              mark of Apple Inc. Android, Google Play and the Google Play logo
+              are trademarks of Google LLC.
+            </p>
+          </div>
         </div>
-        <div>2</div>
+        <div className="mt-12">
+          <div className=" bg-tertiary rounded-[40px]">
+            <div className="relative">
+              <img src={newsletter} alt="newsletter" />
+            </div>
+            <div className="absolute -translate-y-[13rem] translate-x-[2rem] ">
+              <p className="text-[23px] text-primary font-extrabold ">
+                Sign up to our newsletter
+              </p>
+              <p className="text-[15px] text-primary my-5">
+                Receive the latest mobile security news, exclusive <br />
+                discounts & offers straight to your inbox!
+              </p>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address"
+                className="py-1 px-3 text-[15px] rounded-l-2xl"
+              />
+              <button className="py-1 font-bold rounded-r-2xl text-center px-6 bg-primary text-[15px]">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>Part2</div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,60 +23,62 @@ const Navbar = () => {
   };
 
   return (
-    <div className="relative flex items-center justify-between w-full px-[150px] py-4 border-b">
-      {/* left header */}
-      <div className="flex items-center justify-start w-1/2">
-        <div>
-          <img src={logo} alt="logo" />
+    <div className="sticky top-0 bg-white z-[9999]">
+      <div className="relative flex items-center justify-between w-full px-[150px] py-4 border-b ">
+        {/* left header */}
+        <div className="flex items-center justify-start w-1/2">
+          <div>
+            <img src={logo} alt="logo" />
+          </div>
+          <div
+            className="flex items-center ml-12 gap-x-3"
+            onMouseLeave={closeExpandedItems}
+          >
+            {navbarItems.map((item, index) => (
+              <Link to={item.to} key={index}>
+                <div
+                  className={`relative ${
+                    expandedItems === index ? "drop-shadow-md" : ""
+                  }`}
+                  onMouseEnter={() => toggleExpand(index)}
+                >
+                  <p className="text-[16px] gap-x-2 font-medium text-primary flex items-center cursor-pointer">
+                    {item.title}
+                    {item.children && <MdOutlineExpandMore />}
+                  </p>
+                  {item.children && (
+                    <div
+                      className={`p-3 ${
+                        expandedItems === index ? "bg-primary" : "hidden"
+                      } absolute`}
+                    >
+                      {expandedItems === index && (
+                        <ul className="w-[100px]">
+                          {item.children.map((child, childIndex) => (
+                            <li key={childIndex} className="text-white">
+                              {child.title}
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  )}
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
-        <div
-          className="flex items-center ml-12 gap-x-3"
-          onMouseLeave={closeExpandedItems}
-        >
-          {navbarItems.map((item, index) => (
-            <Link to={item.to} key={index}>
-              <div
-                className={`relative ${
-                  expandedItems === index ? "drop-shadow-md" : ""
-                }`}
-                onMouseEnter={() => toggleExpand(index)}
-              >
-                <p className="text-[16px] gap-x-2 font-medium text-primary flex items-center cursor-pointer">
-                  {item.title}
-                  {item.children && <MdOutlineExpandMore />}
-                </p>
-                {item.children && (
-                  <div
-                    className={`p-3 ${
-                      expandedItems === index ? "bg-primary" : "hidden"
-                    } absolute`}
-                  >
-                    {expandedItems === index && (
-                      <ul className="w-[100px]">
-                        {item.children.map((child, childIndex) => (
-                          <li key={childIndex} className="text-white">
-                            {child.title}
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </div>
-                )}
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
 
-      {/* right header */}
-      <div className="flex font-medium  justify-between gap-x-5 text-[16px] text-primary items-center">
-        <div className="flex items-center justify-center gap-x-1">
-          <TfiWorld />
-          EN
+        {/* right header */}
+        <div className="flex font-medium  justify-between gap-x-5 text-[16px] text-primary items-center">
+          <div className="flex items-center justify-center gap-x-1">
+            <TfiWorld />
+            EN
+          </div>
+          <p>Contact Sales</p>
+          <p>Login</p>
+          <CustomButton title="Sign up free" />
         </div>
-        <p>Contact Sales</p>
-        <p>Login</p>
-        <CustomButton title="Sign up free" />
       </div>
     </div>
   );
